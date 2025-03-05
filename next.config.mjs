9c2145286b814a72e7c41d7b39c67c27e7f94020
mdarifahammedreza <mdarifahammedreza@gmail.com>
2025-03-05 19:10:57 +0600

    final

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ["assets.aceternity.com", "images.unsplash.com"], // Add your external image hostnames here
-      },
-};
-
-export default nextConfig;
